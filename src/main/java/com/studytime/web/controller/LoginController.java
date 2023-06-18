7cdf8f52b01dac98c3e7c10426a76bb3c65ebfe6
pjione <a41924250@gmail.com>
2023-06-19 00:16:14 +0900

    로그인 컨트롤러

---
+++ src/main/java/com/studytime/web/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.studytime.web.controller;
+
+import com.studytime.domain.user.Service.LoginService;
+import com.studytime.web.request.LoginRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @PostMapping("/login") //todo 테스트 코드
+    public String login(@RequestBody @Validated LoginRequest loginRequest, HttpServletRequest httpServletRequest){
+
+        String loginId = loginService.login(loginRequest);
+
+        HttpSession session = httpServletRequest.getSession();
+        session.setAttribute("sid", loginId);
+
+        return loginId;
+    }
+}
