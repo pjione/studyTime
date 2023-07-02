d75fa14b8a36bdc42376aae6c436d7c24e947e70
pjione <a41924250@gmail.com>
2023-07-03 00:31:03 +0900

    fix : @EnableJpaAuditing 위치 수정

--- src/main/java/com/studytime/StudytimeApplication.java
+++ src/main/java/com/studytime/StudytimeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class StudytimeApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudytimeApplication.class, args);
---
+++ src/main/java/com/studytime/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.studytime.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
